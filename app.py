1bcc5e4630a61514c07acae34b2fad4c10039230
Valusha Oelofse <142802666+ValOel7@users.noreply.github.com>
2025-03-13 08:44:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import numpy as np
 import streamlit as st
 
 st.title("This is my Streamlit app for 13 March")
-st.heading("This module code is EDAB6808")
-st.subheading("This is a fun and interactive module")
+st.header("This module code is EDAB6808")
+st.subheader("This is a fun and interactive module")
 # Generate random time series data
 time_series = np.random.randn(100)
 
